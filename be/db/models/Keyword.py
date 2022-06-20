7a0635b590e633253a79d43a4283d77cc0b7badb
bayabdi <35993115+bayabdi@users.noreply.github.com>
2022-06-20 15:42:46 +0600

    LLL

---
+++ be/db/models/Keyword.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from db.models import Base
+
+class Keyword(Base):
+    __tablename__ = "keyword"
+    
+    id = Column(Integer, primary_key=True)
+    text = Column(String(500))
+    text_en = Column(String(500))
+    
+    def __repr__(self):
+        return '<Keyword    : {}>'.format(self.id)
--- be/db/models/__init__.py
+++ be/db/models/__init__.py
@@ -2,3 +2,4 @@ from db.base import Base
 from db.models.Job import Job
 from db.models.User import User
 from db.models.Query import Query
+from db.models.Keyword import Keyword
